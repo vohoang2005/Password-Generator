185d98d069985581d7e69649c83d6abbf28d1fcb
vohoang2005 <vohoang2005@gmail.com>
2021-11-11 00:21:56 -0800

    updated while loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
+//Assignment Code + Event Listener to prompt questions when button pushed
+document.querySelector("#generate").addEventListener("click", writePassword);
+
 // Listed all possible outcome.
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symbol = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~", ":", ";", "|", "(", ")", "[", "]", "{", "}", "\""];
+var symbol = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~", ":", ";", "|", "(", ")", "[", "]", "{", "}"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
@@ -10,29 +13,14 @@ var confirmSymbol;
 var confirmUppercase;
 var confirmLowercase;
 
-var generateBtn = document.querySelector("#generate");
-
 function generatePassword() {
-  while (password < 128 && password > 4) 
-  var specialcharacter = window.confirm("Click OK to confirm including special characters");
-  if (window.confirm) {
-    ();
-  } 
+  var confirmLength = window.prompt("How many characters would you like your password to be?");
+  while (confirmLength <= 7 || confirmLength >= 128) {
+    window.alert("Your password length must be between 8-128 characters, please try again.");
+    var confirmLength = window.prompt("How many characters would you like your password to be?")
+    }
+ 
 
-
-
-
-
-// How many characters would you like your password to contain?
-// Click OK to confirm including special characters.
-// Click Ok to confirm including numeric characters.
-// Click ok to confirm including lowercase characters.
-// Click ok to confirm including uppercase characters.
-
-
-  
-
-}
 
 
 // Write password to the #password input
@@ -41,8 +29,5 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+};
